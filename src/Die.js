506a5b48f17f886506b2fba60030b14f30a14a1a
Monicacholico <monicachol@hotmail>
2021-01-05 14:17:08 -0600

    both dice rendering and rolling faces

--- src/Die.js
+++ src/Die.js
@@ -1,29 +1,23 @@
 import React, {Component} from 'react';
-import './App.css';
+import './Die.css';
 
 class Die extends Component {
-    state = {
-        number : 'one'
-    }
-    numOnDice = () => {
-        let dieClasses = ['one', 'two', 'three', 'four', 'five', 'six'];
-        let randNumDie = Math.floor(Math.random() * dieClasses.length);
-        let dieIdx = dieClasses[randNumDie];
-        this.setState({number: dieIdx})
-    };
+    // state = {
+    //     number : 'one'
+    // }
+    // numOnDice = () => {
+    //     let dieClasses = ['one', 'two', 'three', 'four', 'five', 'six'];
+    //     let randNumDie = Math.floor(Math.random() * dieClasses.length);
+    //     let dieIdx = dieClasses[randNumDie];
+    //     this.setState({number: dieIdx})
+    // };
     render() {
-        let displayDie = <i className={`fas fa-dice-${this.state.number}`}></i>;
+        // let displayDie = <i className={`fas fa-dice-${this.props.number}`}></i>;
+        let displayDie = <i className={`fas fa-dice-${this.props.face}`}></i>;
         return (
             <div>
-            <h1>I'm a Die</h1>
             {displayDie}
-            {/* <i className="fas fa-dice-"></i>
-            <i className="fas fa-dice-two"></i>
-            <i className="fas fa-dice-three"></i>
-            <i className="fas fa-dice-four"></i>
-            <i className="fas fa-dice-five"></i>
-            <i className="fas fa-dice-six"></i> */}
-            <button onClick={this.numOnDice}>Die Number</button>
+            {/* <button onClick={this.numOnDice}>Die Number</button> */}
             </div>
         )
     }
